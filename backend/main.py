35fb8872308ae8fb6655ae6f42f866f47b40dda7
天涯古巷 <flavioy@live.com>
2021-12-17 19:06:18 +0800

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -16,11 +16,13 @@ from Levenshtein import ratio
 from PIL import Image
 from numpy import average, dot, linalg
 import numpy as np
-from . import config
-from .tools.reformat import reformat
-from .tools.infer import utility
-from .tools.infer.predict_det import TextDetector
-from .tools.infer.predict_system import TextSystem
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
+from backend import config
+from backend.tools.reformat import reformat
+from backend.tools.infer import utility
+from backend.tools.infer.predict_det import TextDetector
+from backend.tools.infer.predict_system import TextSystem
 import platform
 
 
