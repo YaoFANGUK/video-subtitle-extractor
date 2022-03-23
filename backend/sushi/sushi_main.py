8fe7877bc6ba608272b9993abacb6343ba680f39
Jason <eritpchy@qq.com>
2022-03-23 14:04:01 +0800

    * Sushi: 使用本地临时路径, 提高同步时间轴处理速度 + 处理完成后压缩所有字幕成zip

--- backend/sushi/sushi_main.py
+++ backend/sushi/sushi_main.py
@@ -2,20 +2,20 @@
 import sys 
 import os
 import re
+from pathlib import Path
 from .__main__ import parse_args_and_run
 
-
-def subtitle_sync(argv):
+def subtitle_sync(argv, opts=None):
     videos = []
     isAss = False
     if len(argv) < 3:
         print("参数不足, 程序退出")
         exit()
     for arg in argv:
-        if re.search('\\.(ass)$', arg, flags= re.I) is not None:
+        if re.search('\\.(ass)$', arg, flags=re.I) is not None:
             subtitle = arg
             isAss = True
-        elif re.search('\\.(srt)$', arg, flags= re.I) is not None:
+        elif re.search('\\.(srt)$', arg, flags=re.I) is not None:
             subtitle = arg
         else:
             videos.append(arg)
@@ -33,8 +33,18 @@ def subtitle_sync(argv):
     print(("HD:", hdVideo))
     print(("BD:", bdVideo))
 
-    hdWav = hdVideo + '.wav'
-    bdWav = bdVideo + '.wav'
+    temp_dir = None
+    if opts is not None:
+        if "temp" in opts:
+            temp_dir = opts["temp"]
+
+    if temp_dir is not None:
+        hdWav = os.path.join(temp_dir, os.path.basename(hdVideo) + '.wav')
+        bdWav = os.path.join(temp_dir, os.path.basename(bdVideo) + '.wav')
+        Path(temp_dir).mkdir(parents=True, exist_ok=True)
+    else:
+        hdWav = hdVideo + '.wav'
+        bdWav = bdVideo + '.wav'
 
     os.system('ffmpeg -y -i "{0}" "{1}"'.format(hdVideo, hdWav))
     os.system('ffmpeg -y -i "{0}" "{1}"'.format(bdVideo, bdWav))
@@ -58,7 +68,7 @@ def subtitle_sync(argv):
     print(("finish", output))
 
 if __name__ == '__main__':
-    subtitle_sync(sys.argv[1:])
+    subtitle_sync(sys.argv[1:], None)
 
 
 
